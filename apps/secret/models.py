714df38473cb793417765c3140008726635af764
Matthew Neall <neallmr@gmail.com>
2016-09-19 10:35:31 -0700

    add form to index and secret model to models

--- apps/secret/models.py
+++ apps/secret/models.py
@@ -2,4 +2,8 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Secrets(models.Model):
+	secret = models.CharField(max_length=500)
+	likes = models.IntegerField(blank=True, null=True)
+	created_at = models.DateField(auto_now_add=True)
+	updated_at = models.DateField(auto_now=True)
